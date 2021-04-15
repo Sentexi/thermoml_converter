0fb169669e58bf406283e8974d1fa756cb9b8e33
Andrey Belkin <communicationbel@gmail.com>
2021-04-15 02:16:12 +0200

    implemented data mapping cml to thermoml

--- cml2thermoml.py
+++ cml2thermoml.py
@@ -18,8 +18,15 @@ def create_xml_subelement_with_list(parent,name,list):
     list.append(child)
     
     return child
+    
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
 
-def create_data_entry(data_csv):
+def create_data_entry(cmldata):
 
     #data_csv, nCompounds, nVariables, varTypes, nDatapoints, nDatapoints_per_entry = preproc_data_entry(data_csv)
     
@@ -32,7 +39,7 @@ def create_data_entry(data_csv):
     #Info about the Dataframe
     nPureOrMixtureDataNumber = create_xml_subelement_with_list(PureOrMixtureData,"nPureOrMixtureDataNumber",Elements)
     
-    for n in range(nCompounds):
+    for n in range(2): #CML probably provides only 2 compounds
      
         Component = create_xml_subelement_with_list(PureOrMixtureData,"Component",empty)
         RegNum = create_xml_subelement_with_list(Component,"RegNum",empty)
@@ -83,58 +90,71 @@ def create_data_entry(data_csv):
     nConstrDigits = create_xml_subelement_with_list(Constraint,"nConstrDigits",Elements)
     
     #Variable declaration
-    for i in range(nVariables-1):
+    for i in range(3):
         Variable = create_xml_subelement_with_list(PureOrMixtureData,"Variable",empty)
         nVarNumber = create_xml_subelement_with_list(Variable,"nVarNumber",Elements)
+        nVarNumber.text = str(i+1)
         VariableID = create_xml_subelement_with_list(Variable,"VariableID",empty)
         VariableType = create_xml_subelement_with_list(VariableID,"VariableType",empty)
-        #TODO: Choose between temperature, composition, or any other ThermoML tags
-        print(nVariables)
-        if varTypes[i] == "Temperature, K":
+        if i == 0:
             eTemperature = create_xml_subelement_with_list(VariableType,"eTemperature",Elements)
+            eTemperature.text =  "Temperature, K"
         else:
             eComponentComposition = create_xml_subelement_with_list(VariableType,"eComponentComposition",Elements)
+            eComponentComposition.text = "Mole fraction"
             #For component composition only: RegNum of compound as reference
             RegNum = create_xml_subelement_with_list(VariableID,"RegNum",empty) 
             nOrgNum = create_xml_subelement_with_list(RegNum,"nOrgNum",Elements) 
         #Phase info again
         VarPhaseID = create_xml_subelement_with_list(Variable,"VarPhaseID",empty)
         eVarPhase = create_xml_subelement_with_list(VarPhaseID,"eVarPhase",Elements)    
+        eVarPhase.text = "liquid"
 
+    nDatapoints = len(cmldata)
     
     for i in range(nDatapoints):
         NumValues = create_xml_subelement_with_list(PureOrMixtureData,"NumValues",empty)
 
         #loops for each variable
-        for j in range(nDatapoints_per_entry):
+        for j in range(3):
             VariableValue = create_xml_subelement_with_list(NumValues,"VariableValue",empty)
             nVarNumber = create_xml_subelement_with_list(VariableValue,"nVarNumber",Elements)
+            nVarNumber.text = str(j+1)
             nVarValue = create_xml_subelement_with_list(VariableValue,"nVarValue",Elements)
+            nVarValue.text = str(cmldata[i][-j])
             nVarDigits = create_xml_subelement_with_list(VariableValue,"nVarDigits",Elements)
+            nVarDigits.text = str(0) #CML provides no significant digits
         
         #once per datapoint add property value
         PropertyValue = create_xml_subelement_with_list(NumValues,"PropertyValue",empty)
         nPropNumber = create_xml_subelement_with_list(PropertyValue,"nPropNumber",Elements)
+        nPropNumber.text = str(1)
         nPropValue = create_xml_subelement_with_list(PropertyValue,"nPropValue",Elements)
+        nPropValue.text = str(cmldata[i][2])
         nPropDigits = create_xml_subelement_with_list(PropertyValue,"nPropDigits",Elements)
+        nPropDigits.text = str(len(cmldata[i][3].split("."))) #CML provides us with significant digits for the measured value
         CombinedUncertainty = create_xml_subelement_with_list(PropertyValue,"CombinedUncertainty",empty)
         nCombUncertAssessNum = create_xml_subelement_with_list(CombinedUncertainty,"nCombUncertAssessNum",Elements)
+        nCombUncertAssessNum.text = str(1)
         nCombExpandUncertValue = create_xml_subelement_with_list(CombinedUncertainty,"nCombExpandUncertValue",Elements)
+        nCombExpandUncertValue.text = str(0) #Again dummy values
         
-    
+    '''
     for i in range(len(data_csv)):
         print(len(Elements))
         print(len(data_csv))
         print(Elements[i])
         #print(data_csv[i][1])
         Elements[i].text = data_csv[i][1]
+    '''
+    #No looping since all text is entered above
         
     final = prettify(PureOrMixtureData)
     print(final)
 
     return final[23:]
 
-def create_thermoml(folderpath,cmlfile):
+def create_thermoml(folderpath,cmlfile,cmldata):
     #Declare header and footer
     header = "<!-- Generated by the NIST, Thermodynamics Research Center (http://www.trc.nist.gov) --> \n \
     <!-- This ThermoML file was generated by thermoml converter (https://github.com/Sentexi/thermoml_converter/) --> \n \
@@ -188,17 +208,18 @@ def create_thermoml(folderpath,cmlfile):
         all_datapoints += datapoint
     '''
     
-    all_datapoints = create_data_entry(datapoint_data)
+    all_datapoints = create_data_entry(cmldata)
     
     final_name = cmlfile.split(".")[0] + "_thermoml.xml"
     
     C.writefile(header + version_info + citation + all_compounds + all_datapoints + footer, final_name)
 
 if __name__ == "__main__":
-    #create_thermoml("CML","CML_exp_sim.xml")
-    
     tree = ET.parse('CML_exp_sim.xml')
     root = tree.getroot()
+    
+    cmldata = []
+    
     for exp in root[0][0]:
         #print(exp.tag, exp.attrib)
         DOI = exp[0][0][0].text #this is just for consistency, no need to read in the DOI each time
@@ -209,5 +230,11 @@ if __name__ == "__main__":
         mol_DES = exp[1][0][0].text #the molar ratio of the DES
         mol_water = exp[1][1][0].text #the molar ratio of water
         T = exp[1][2][0].text #temperature in K
+        
+        cmldata.append([DOI,ID,val_density,str(val_error),mol_DES,mol_water,T]) #eventually this could be a class
+        
+    create_thermoml("CML","CML_exp_sim.xml",cmldata)
+        
+    
         
         
